7c8f3bf14d52c7ec20d1ad02b6c4bd6ca057ad82
Danny Fsl <dannyh_fsl@yahoo.com>
2025-08-27 16:49:40 +1000

    chore(network): add provider and define existing resource group

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "8b6001cd-a329-4999-8dbb-3b3261bb100a"
+}
